c464645cc303385392b34ab4d841471bf21243e0
Audrey Roy <audreyr@gmail.com>
2013-05-18 13:17:15 +0200

    Enabled admin at admin/ in example. Put admin2 at admin2/.

--- example/example/settings.py
+++ example/example/settings.py
@@ -115,8 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'djadmin2',
--- example/example/urls.py
+++ example/example/urls.py
@@ -1,17 +1,15 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', include('djadmin2.urls')),
+    url(r'^admin2/$', include('djadmin2.urls')),
     # url(r'^example/', include('example.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
