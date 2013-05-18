bfc61eadcbc40878fa5d6133298a48c6f52ea94f
Ludvig Wadenstein <ludvig.wadenstein@codemill.se>
2013-05-18 18:24:21 +0200

    Added basic test for api index view

---
+++ example/blog/tests/test_apiviews.py
@@ -0,0 +1,18 @@
+from django.utils import unittest
+from django.test.client import RequestFactory
+
+
+from djadmin2 import apiviews
+from ..models import Post
+
+
+class ViewTest(unittest.TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+
+class IndexViewModelListCreateAPIViewTest(ViewTest):
+    def test_response_ok(self):
+        request = self.factory.get('/admin/api/blog/post/')
+        response = apiviews.ModelListCreateAPIView.as_view(model=Post)(request)
+        self.assertEqual(response.status_code, 200)
