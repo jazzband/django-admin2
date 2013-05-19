2a28417f0f2a85dc17dd060ba27a49207238a523
Ludvig Wadenstein <ludvig.wadenstein@codemill.se>
2013-05-19 10:13:30 +0200

    Added test for unicode field in rest api

--- example/blog/tests/test_apiviews.py
+++ example/blog/tests/test_apiviews.py
@@ -12,7 +12,16 @@ class ViewTest(unittest.TestCase):
 
 
 class IndexViewModelListCreateAPIViewTest(ViewTest):
+
     def test_response_ok(self):
         request = self.factory.get('/admin/api/blog/post/')
         response = apiviews.ModelListCreateAPIView.as_view(model=Post)(request)
         self.assertEqual(response.status_code, 200)
+
+    def test_list_includes_unicode_field(self):
+        Post.objects.create(title='Foo', body='Bar')
+        request = self.factory.get('/admin/api/blog/post/')
+        response = apiviews.ModelListCreateAPIView.as_view(model=Post)(request)
+        response.render()
+
+        self.assertIn('"unicode": "Foo"', response.content)
