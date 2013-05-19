585d4d9e8e62c683011379572f8b431c9041202a
Ludvig Wadenstein <ludvig.wadenstein@codemill.se>
2013-05-19 10:56:11 +0200

    Just commiting to be able to merge

--- example/blog/tests/test_apiviews.py
+++ example/blog/tests/test_apiviews.py
@@ -1,5 +1,6 @@
 from django.utils import unittest
 from django.test.client import RequestFactory
+from django.core.urlresolvers import reverse
 
 
 from djadmin2 import apiviews
@@ -11,17 +12,26 @@ class ViewTest(unittest.TestCase):
         self.factory = RequestFactory()
 
 
-class IndexViewModelListCreateAPIViewTest(ViewTest):
+class ModelListCreateAPIViewTest(ViewTest):
 
     def test_response_ok(self):
-        request = self.factory.get('/admin/api/blog/post/')
-        response = apiviews.ListCreateAPIView.as_view(model=Post)(request)
+        request = self.factory.get(reverse('admin2:api-index', args=['blog', 'post']))
+        response = apiviews.ModelListCreateAPIView.as_view(model=Post)(request)
         self.assertEqual(response.status_code, 200)
 
     def test_list_includes_unicode_field(self):
         Post.objects.create(title='Foo', body='Bar')
-        request = self.factory.get('/admin/api/blog/post/')
+        request = self.factory.get(reverse('admin2:api-index'), args=['blog', 'post'])
         response = apiviews.ModelListCreateAPIView.as_view(model=Post)(request)
         response.render()
 
         self.assertIn('"unicode": "Foo"', response.content)
+
+
+class ModelRetrieveUpdateDestroyAPIViewTest(ViewTest):
+
+    def test_response_ok(self):
+        post = Post.objects.create(title='Foo', body='Bar')
+        request = self.factory.get(reverse('admin2:api-detail', args=['blog', 'post', post.pk]))
+        response = apiviews.ModelRetrieveUpdateDestroyAPIView.as_view(model=Post)(request)
+        self.assertEqual(response.status_code, 200)
