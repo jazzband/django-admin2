69bf7b497eed6b0f2903ef416d0c5fd646729dfd
Daniel Greenfeld <pydanny@gmail.com>
2013-05-19 13:10:02 +0200

    Get djang-debug-toolbar into the demo

--- example/example/settings.py
+++ example/example/settings.py
@@ -156,3 +156,19 @@ LOGGING = {
 
 
 ADMIN2_THEME_DIRECTORY = "admin2/bootstrap/"
+
+
+########## TOOLBAR CONFIGURATION
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+INTERNAL_IPS = ('127.0.0.1',)
+
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+########## END TOOLBAR CONFIGURATION
