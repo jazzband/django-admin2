a5cb74fb6333876fad2d9556987bf1855150e655
Daniel Greenfeld <pydanny@gmail.com>
2013-05-22 17:58:29 +0200

    Update django.contrib.admin reference file to provide reference for this pull request

--- example/blog/admin.py
+++ example/blog/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+
 from .models import Post, Comment
 
-admin.site.register(Post)
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentInline, ]
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
