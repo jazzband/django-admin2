a802dfe8ae5bb3b6ba03c4566d6a61520f2d12b1
Ludvig Wadenstein <ludvig.wadenstein@codemill.se>
2013-06-02 20:58:27 +0200

    Replaced actions select with button dropdown.

--- djadmin2/static/themes/bootstrap/js/actions.js
+++ djadmin2/static/themes/bootstrap/js/actions.js
@@ -9,8 +9,8 @@ $(function() {
 		for (var ix = 0; ix < selectCheckboxen.length; ix++) {
 			if ($(selectCheckboxen[ix]).prop('checked')) {
 				count++;
-			};
-		};
+			}
+		}
 		selectAllCheckbox.prop('checked', count == selectCheckboxen.length);
 		selectedCount.text(count);
 	};
@@ -23,4 +23,15 @@ $(function() {
 	selectCheckboxen.click(function(e) {
 		updateSelectedCount();
 	});
+
+    
+    var actionDropdownLink = element.find('.dropdown-menu a');
+    actionDropdownLink.click(function (e) {
+        e.preventDefault();
+        var form = $(this).closest('form');
+        form.find('input[name="' + $(this).data('name') + '"]').val(
+            $(this).data('value'));
+        form.submit();
+    });
+
 });
