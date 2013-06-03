45855f9b501caa6651c02c1c6842f78fecba0f9f
Daniel Greenfeld <pydanny@gmail.com>
2013-06-03 23:30:48 +0200

    German to English :wink:

--- djadmin2/static/themes/bootstrap/js/actions.js
+++ djadmin2/static/themes/bootstrap/js/actions.js
@@ -1,26 +1,26 @@
 $(function() {
 	var element = $("#model-list");
 	var selectAllCheckbox = element.find('.model-select-all');
-	var selectCheckboxen = element.find('.model-select');
+	var selectCheckbox = element.find('.model-select');
 	var selectedCount = element.find('#selected-count');
 
 	var updateSelectedCount = function() {
 		var count = 0;
-		for (var ix = 0; ix < selectCheckboxen.length; ix++) {
-			if ($(selectCheckboxen[ix]).prop('checked')) {
+		for (var ix = 0; ix < selectCheckbox.length; ix++) {
+			if ($(selectCheckbox[ix]).prop('checked')) {
 				count++;
 			}
 		}
-		selectAllCheckbox.prop('checked', count == selectCheckboxen.length);
+		selectAllCheckbox.prop('checked', count == selectCheckbox.length);
 		selectedCount.text(count);
 	};
 
 	selectAllCheckbox.click(function(e) {
-		selectCheckboxen.prop('checked', this.checked);
+		selectCheckbox.prop('checked', this.checked);
 		updateSelectedCount();
 	});
 
-	selectCheckboxen.click(function(e) {
+	selectCheckbox.click(function(e) {
 		updateSelectedCount();
 	});
 
