b1c7089556c7e09a81a9ad86314e8abfaf149927
Andrews Medina <andrewsmedina@gmail.com>
2013-06-04 23:07:09 -0300

    Enabling admin for example2.

--- example2/example2/urls.py
+++ example2/example2/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
 
 import djadmin2
 
@@ -6,4 +9,5 @@ djadmin2.default.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin2/', include(djadmin2.default.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
