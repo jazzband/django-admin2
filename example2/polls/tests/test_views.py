25a9d72322a4a51c39fa84f3ac213fd8a39d71ab
Andrews Medina <andrewsmedina@gmail.com>
2013-06-05 22:35:57 -0300

    Added tests for example2 admin index.

---
+++ example2/polls/tests/test_views.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+from django.test import TestCase, Client
+
+
+class BaseIntegrationTest(TestCase):
+    """
+    Base TestCase for integration tests.
+    """
+    def setUp(self):
+        self.client = Client()
+        self.user = get_user_model()(username='user', is_staff=True,
+                                     is_superuser=True)
+        self.user.set_password("password")
+        self.user.save()
+        self.client.login(username='user', password='password')
+
+
+class AdminIndexTest(BaseIntegrationTest):
+    def test_view_ok(self):
+        response = self.client.get(reverse("admin2:dashboard"))
+        self.assertContains(response, reverse("admin2:polls_poll_index"))
