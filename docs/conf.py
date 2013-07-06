2282f9681d30529658d1d7836b636201dd3e0f62
Danilo Bargen <gezuru@gmail.com>
2013-07-06 13:54:48 +0200

    Get rid of all Sphinx warnings

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,8 @@ sys.path.append(ext_path)
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['djangodocs', 'sphinx.ext.autodoc', 'sphinx.ext.intersphinx']
+extensions = ['djangodocs', 'sphinx.ext.autodoc', 'sphinx.ext.intersphinx',
+'sphinx.ext.todo']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
