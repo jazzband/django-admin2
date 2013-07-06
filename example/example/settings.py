807b3776fb7cea0d44507bce8ac4fc374c024740
Audrey Roy <audreyr@gmail.com>
2013-07-06 10:57:00 +0200

    Better example project home. Close #241.

--- example/example/settings.py
+++ example/example/settings.py
@@ -1,4 +1,7 @@
 # Django settings for example project.
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -67,6 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, "static"),
 )
 
 # List of finder classes that know how to find static files in
