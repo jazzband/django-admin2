8def10b2daa4082d749c30b86e5574b3fc012247
d1ffuz0r <d1fffuz0r@gmail.com>
2013-07-09 13:08:39 +0700

    Fixed #292 added missing default columns and filters

--- djadmin2/admin2.py
+++ djadmin2/admin2.py
@@ -34,7 +34,12 @@ class UserSerializer(Admin2APISerializer):
 class UserAdmin2(djadmin2.ModelAdmin2):
     create_form_class = UserCreationForm
     update_form_class = UserChangeForm
-    search_fields = ('username', 'groups__name')
+    search_fields = ('username', 'groups__name', 'first_name', 'last_name',
+                     'email')
+
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    ordering = ('username',)
 
     api_serializer_class = UserSerializer
 
