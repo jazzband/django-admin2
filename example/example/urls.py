944baf62dbd5853fe3b6eed4c1ea52c3cc8bbc80
Daniel Greenfeld <pydanny@gmail.com>
2013-07-14 12:36:56 +0200

    Cleanup of example templates

--- example/example/urls.py
+++ example/example/urls.py
@@ -13,6 +13,6 @@ urlpatterns = patterns('',
     url(r'^admin2/', include(djadmin2.default.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', BlogListView.as_view(template_name="blog/blog_list.html"), name='blog_list'),
-    url(r'^blog_detail(?P<pk>\d+)/$', BlogDetailView.as_view(template_name="blog/blog_detail.html"), name='blog_detail'),
-    url(r'^$', BlogListView.as_view(template_name="blog/blog_list.html"), name='blog_list'),
+    url(r'^blog/detail(?P<pk>\d+)/$', BlogDetailView.as_view(template_name="blog/blog_detail.html"), name='blog_detail'),
+    url(r'^$', BlogListView.as_view(template_name="blog/home.html"), name='home'),
 )
