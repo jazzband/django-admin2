42ed91592ca60f14a04b9d8ea7e90d09aaf1fac8
Henry Clifford <h.a.clifford@gmail.com>
2013-10-21 13:33:51 -0400

    add support for dropdowns

--- djadmin2/static/themes/bootstrap/js/base.js
+++ djadmin2/static/themes/bootstrap/js/base.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     /**
-     * Dynamicaly adds button to each input element
+     * Dynamically adds button to each input element
      * Required by filter section to allow form to be submitted
      */
     $("#filter_form").find('input').each(function(){
@@ -9,9 +9,11 @@ $(document).ready(function() {
 
         var btn = input_field.after('<button class="btn">Go</button>');
     });
-
+    /**
+     * If the there's a select in the filters; filter when it's changed
+     */
     $('#filter_form select').on('change', function(e){
-        debugger;
+        $(this).closest('form').submit();
     });
 
 });
