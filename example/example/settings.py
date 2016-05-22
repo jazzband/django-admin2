e74593d4b4fd65ed744d16a88174505a68aab825
arthur <arthur@wallstreetweb.net>
2016-05-22 10:20:17 +0200

    Use DRF PageNumberPagination to finish fixing DRF tests

--- example/example/settings.py
+++ example/example/settings.py
@@ -146,3 +146,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
 
 ADMIN2_THEME_DIRECTORY = "djadmin2theme_default"
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
