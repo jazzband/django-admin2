8636f6ce9851a876a8b5323d6925d4e54ca95fd7
Asif Saif Uddin <auvipy@gmail.com>
2018-09-03 14:54:37 +0600

    template dir settings

--- example/example/settings.py
+++ example/example/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'example.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
