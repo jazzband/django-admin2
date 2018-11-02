0e28a67b22909c29982119db4b0da412bdd7a5b1
Asif Saif Uddin <auvipy@gmail.com>
2018-11-02 22:26:17 +0600

    empty list in template dirs

--- example/example/settings.py
+++ example/example/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'example.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
